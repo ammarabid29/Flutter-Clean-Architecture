4bdcd715c2c730baceb68204d22474d6c7aa0dfe
ammarabid29 <aammarawan786@gmail.com>
2024-12-31 13:16:54 +0500

    Completed Data & Domain part

---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_architecture/core/constants/constants.dart';
+import 'package:flutter_clean_architecture/features/daily_news/data/models/article.dart';
+import 'package:retrofit/retrofit.dart';
+
+// run this command
+// flutter pub run build_runner build
+
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: newsAPIBaseURL)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+
+  @GET("/top-headlines")
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
+    @Query("apikey") String? apiKey,
+    @Query("country") String? country,
+    @Query("category") String? category,
+  });
+}
